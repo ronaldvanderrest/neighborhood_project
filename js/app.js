d41c706ebfef0cd1d1ccfc89821d9a619cb192e7
Ronald van der Rest <ronaldvanderrest@live.nl>
2018-03-12 17:45:33 +0100

    rename javascript files

--- js/app.js
+++ js/app.js
@@ -9,12 +9,12 @@ function initMap() {
     center: {lat: 40.7413549, lng: -73.9980244},
     zoom: 13,
     mapTypeControl: true
-  });
+	});
+	
   ko.applyBindings(new ViewModel())
 };
 
-var markers = []
-
+// Model
 // These are the initial locations for the application to be shown
 var locations = [
     {title: 'Diergaarde Blijdorp', location: {lat: 51.92788645142215, lng: 4.445233869337291}},
@@ -24,29 +24,40 @@ var locations = [
     {title: 'Bokaal', location: {lat: 51.92247410754488, lng: 4.488164449775141}}
 ];
 
-// Making the titles of the locations into an Knockout observable
+// Template to make Locations into an observable
 var Location = function(data){
     this.title = ko.observable(data.title);
     this.location = ko.observable(data.location);
 };
 
+
 var ViewModel = function() {
-    var self = this;
+	var self = this;
 
-    this.locationList = ko.observableArray([]);
+	// Create a boundary variable
+	var bounds = new google.maps.LatLngBounds();
 
-    locations.forEach(function(item){
-        self.locationList.push( new Location(item));
-    });
+	// create an empty observable array
+	this.locationList = ko.observableArray([]);
 
-    locations.forEach(function(location) {
-        // Create a marker per location, and put into markers array.
-        var marker = new google.maps.Marker({
-          map: map,
-        	position: location.location,
-          title: location.title,
-          animation: google.maps.Animation.DROP
-				});
-				location.marker = marker;
-    });
-	};
+	// fill the empty observable array with the items from the model
+	locations.forEach(function(item){
+			self.locationList.push( new Location(item));
+	});
+
+	// iterate over the locations and create a marker. Also including it in the model
+	locations.forEach(function(location) {
+			// Create a marker per location, and put into markers array.
+			var marker = new google.maps.Marker({
+				map: map,
+				position: location.location,
+				title: location.title,
+				animation: google.maps.Animation.DROP
+			});
+			location.marker = marker;
+			// extend the boundary if the marker is outside it
+			bounds.extend(marker.position);
+	});
+	// Apply the boundary to the map
+  map.fitBounds(bounds);
+};
