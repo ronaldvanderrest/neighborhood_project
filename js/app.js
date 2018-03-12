84393c5ecca51ddbba16051ee00b898801efcfae
Ronald van der Rest <ronaldvanderrest@live.nl>
2018-03-12 21:41:38 +0100

    include foursquare api

--- js/app.js
+++ js/app.js
@@ -14,6 +14,66 @@ function initMap() {
   ko.applyBindings(new ViewModel())
 };
 
+// This function populates the infowindow specified with information of the 
+// clicked location
+function populateInfoWindow(marker, infowindow) {
+	// check if the infowindow is already opened at the clicked marker
+	if (infowindow.marker != marker) {
+		// put the infowindow at the clicked marker
+		infowindow.marker = marker;
+		// lat and lon values from the marker for foursquare api search
+		var lat = marker.getPosition().lat();
+		var lng = marker.getPosition().lng();
+		// set credentials for foursquare API (better to do on server-side though)
+		var clientID="LI40PW0QW52Y4FZH00KGHABEF1DYGC1XDXZQZPP1NWX040CH";
+		var clientSecret="TOE0DA20IJWW0LSG544ULBTL1VVKWDCU3J14H3XVF2FJQ5TF";
+		// set foursquare url for getting details
+		fsVenueDetailsUrl='https://api.foursquare.com/v2/venues/search?ll=' + lat + ',' + lng + '&query=' + encodeURIComponent(marker.title) + '&client_id=' + clientID + '&client_secret=' + clientSecret + '&v=20180312';
+		// set html variables to be filled by the ajax requests
+		var venueDetailsHtml;
+		var venuePhoto;
+
+		// perform AJAX request to get venue details
+		$.getJSON(fsVenueDetailsUrl, function(data){
+			var venue = data.response.venues[0];
+			var venueName = venue.name;
+			var venueId = venue.id;
+			var venueCategory = venue.categories[0].name;
+
+			var fsPhotosUrl='https://api.foursquare.com/v2/venues/' + venueId + '/photos?' + '&client_id=' + clientID + '&client_secret=' + clientSecret + '&v=20180312';
+			// doing a synchronous request to load photo within the initial asynchronous request.
+			// this prevents race condition
+			$.ajax({
+				async: false,
+				url: fsPhotosUrl
+			}).done(function(data){
+				var debug = data.response.photos.items[0].suffix;
+				var photoPrefix = data.response.photos.items[0].prefix;
+				var photoSuffix = data.response.photos.items[0].suffix;
+				venuePhoto = photoPrefix +'height100'+photoSuffix;
+			})
+			.fail(function( jqxhr, textStatus, error ) {
+				var err = textStatus + ", " + error;
+				console.log( "Request Failed: " + err );
+			});
+
+
+			venueDetailsHtml = '<div><h3>' + venueName + '</h3>' + '<h4>' + venueCategory + '</h4><img src=' + venuePhoto + '></div>';
+			infowindow.setContent(venueDetailsHtml);
+		}).fail(function( jqxhr, textStatus, error ) {
+			var err = textStatus + ", " + error;
+			console.log( "Request Failed: " + err );
+			infowindow.setContent('<div><h5>Failed to request Foursquare</h5></div>');
+		});
+		// set content in the infowindow
+		infowindow.open(map, marker);
+		// Make sure the marker property is cleared if the infowindow is closed.
+		infowindow.addListener('closeclick',function(){
+		  infowindow.setMarker = null;
+		});
+	  }
+}
+
 // Model
 // These are the initial locations for the application to be shown
 var locations = [
@@ -37,6 +97,9 @@ var ViewModel = function() {
 	// Create a boundary variable
 	var bounds = new google.maps.LatLngBounds();
 
+	// Create an infowindow for when you select a location
+	var infoWindow = new google.maps.InfoWindow();
+
 	// create an empty observable array
 	this.locationList = ko.observableArray([]);
 
@@ -57,7 +120,10 @@ var ViewModel = function() {
 			location.marker = marker;
 			// extend the boundary if the marker is outside it
 			bounds.extend(marker.position);
+			marker.addListener('click', function() {
+				populateInfoWindow(this, infoWindow);
+			  });
 	});
 	// Apply the boundary to the map
-  map.fitBounds(bounds);
+	map.fitBounds(bounds);
 };
